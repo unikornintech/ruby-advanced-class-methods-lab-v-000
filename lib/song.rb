22791fbc3747ad993768e8b06a490a98522c61ce
Lauren Days <lmchambers0420@gmail.com>
2019-02-06 23:48:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,23 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create 
+    song = self.new 
+    @@all << song 
+    song 
+  end 
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name 
+    song 
+  end 
+  
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    @@all << song 
+    song 
+  end 
 
 end
