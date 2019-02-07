2b0cc9e346f7d520d365a26f05cd05c99c2f0c54
Lauren Days <lmchambers0420@gmail.com>
2019-02-07 00:04:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
       song
     end 
     
-    def self.create_from_filename
+    def self.create_from_filename(filename)
       song = self.new_from_filename(filename)
       @all << song 
       song 
